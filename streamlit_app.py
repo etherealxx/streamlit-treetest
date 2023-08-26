662c1b72bd187ef59445624f753017f154c293fb
Etherealxx <gwathon3@gmail.com>
2023-08-26 14:56:45 +0700

    wtf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,25 @@
 #autoabsen
 
 import streamlit as st
-import subprocess
+# import subprocess
 
-tree = subprocess.getoutput('tree /')
-st.write(tree)
+# tree = subprocess.getoutput('tree /')
+
+import os
+
+# st.write("pat")
+
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        st.write('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            st.write('{}{}'.format(subindent, f))
+
+
+start_directory = "/mount/src"
+list_files(start_directory)
+
+# st.write(tree)
